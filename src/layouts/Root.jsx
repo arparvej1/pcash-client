1645e3bcf2c6205424fcc4604c7aae70edfa500e
arparvej1 <parvej8pro@gmail.com>
2024-07-18 13:39:32 +0600

    footer adjust

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -16,21 +16,21 @@ const Root = () => {
     <>
       <div className="max-w-screen-xl mx-5 xl:px-5 2xl:px-0 xl:mx-auto border-4 border-green-400">
         <div className="max-w-full bg-neutral">
-          <Navbar></Navbar>
+          <Navbar />
         </div>
         <div className="md:grid md:grid-cols-6">
           <div className="col-span-1">
-            <Sidebar></Sidebar>
+            <Sidebar />
           </div>
-          <div className="md:col-span-5 max-w-screen-xl 2xl:px-0 bg-green-400">
-            <div className="p-3">
-              <Outlet></Outlet>
+          <div className="md:col-span-5 max-w-screen-xl 2xl:px-0 bg-green-400 flex flex-col min-h-fit">
+            <div className="flex-grow p-3">
+              <Outlet />
             </div>
-              <footer className="footer footer-center bg-base-300 text-base-content p-4">
-                <aside>
-                  <p>Copyright © {new Date().getFullYear()} - All right reserved by pCash</p>
-                </aside>
-              </footer>
+            <footer className="footer footer-center bg-base-300 text-base-content p-4">
+              <aside>
+                <p>Copyright © {new Date().getFullYear()} - All right reserved by pCash</p>
+              </aside>
+            </footer>
           </div>
         </div>
       </div>
